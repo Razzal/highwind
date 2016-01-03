a3d813f202fb2806e8e9ce5b874e7bf506284ffc
Razzal <jayhawkfanron@gmail.com>
2016-01-02 20:56:31 -0600

    init commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name highwindApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the highwindApp
+ */
+angular.module('highwindApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
